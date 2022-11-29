ca0a9504f35e913359ed9c354bedf599a4a3e6b8
Adam Teo <adam.teo321@gmail.com>
2022-11-29 13:43:42 -0600

    Add on_ready and help functions

--- bot.py
+++ bot.py
@@ -21,3 +21,21 @@ conn = sqlite3.connect('tracking.db')
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS tracking(subreddits TEXT, keywords TEXT, channel INTEGER)")
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name="help")
+async def help(ctx):
+    embeddedHelp = discord.Embed(title="Help", description="List of commands", color=discord.Color.light_gray())
+    embeddedHelp.add_field(name="`$start`", value="Starts tracking", inline=False)
+    embeddedHelp.add_field(name="`$end`", value="Stops tracking", inline=False)
+    embeddedHelp.add_field(name="`$track`", value="Track a subreddit or keyword. Separate multiple subreddits/keywords with a space.", inline=False)
+    embeddedHelp.add_field(name="`$untrack`", value="Untrack a subreddit. Separate multiple subreddits with a space.", inline=False)
+    embeddedHelp.add_field(name="`$add`", value="Add a keyword. Separate multiple keywords with a space.", inline=False)
+    embeddedHelp.add_field(name="`$remove`", value="Remove a keyword. Separate multiple keywords with a space.", inline=False)
+    embeddedHelp.add_field(name="`$current`", value="Show currently tracked subreddits/keywords.", inline=False)
+    embeddedHelp.add_field(name="`$setchannel`", value="Set the channel to send posts to. Only one channel can be set at a time.", inline=False)
+    embeddedHelp.add_field(name="`$help`", value="Show this help message.", inline=False)
+
+    await ctx.send(embed=embeddedHelp)
